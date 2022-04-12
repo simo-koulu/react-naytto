647d3bb5efcf561c4967d83fd0f6e76ec0411ec0
Simo <sk3626@edu.turku.fi>
2022-04-13 02:55:48 +0300

    testi

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <button>Kirjaudu sisään</button>
+        <h1> teis tesit </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+document.body.style = 'background: hsl(0, 0%, 13%)';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
